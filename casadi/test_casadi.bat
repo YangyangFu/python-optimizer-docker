fc14b4872de5f2c38c3e8409e35438239c38babe
Yangyang Fu <yangyang.fu@tamu.edu>
2021-02-24 19:14:16 -0600

    Added casadi wrapper for ipopt

---
+++ casadi/test_casadi.bat
@@ -0,0 +1,11 @@
+docker run --name casadi_con^
+      --user=root^
+	  --detach=false^
+	  -e DISPLAY=${DISPLAY}^
+	  -v /tmp/.X11-unix:/tmp/.X11-unix^
+	  --rm^
+	  -v %CD%:/mnt/shared^
+	  -i^
+      -t^
+	  casadi /bin/bash -c "cd /mnt/shared && python /mnt/shared/test_casadi.py"
+
